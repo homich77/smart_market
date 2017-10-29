a7c8234bb71cf59fb61423a1823b4c42fbff970f
lemki <lwwwwwww0@gmail.com>
2017-10-29 22:12:08 +0200

    admin settings

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+    inlines = [ProductImageInline]
+
+    class Meta:
+        model = Product
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in ProductImage._meta.fields]
+
+    class Meta:
+        model = ProductImage
+
+admin.site.register(ProductImage, ProductImageAdmin)

263fff753d9760ae1b40f426b8c0179cd4021a18
lemki <lwwwwwww0@gmail.com>
2017-11-02 10:54:32 +0200

    add product details page

--- product/views.py
+++ product/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = 'product/product_detail.html'

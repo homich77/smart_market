324605eeb652afc9465091264f5ded4a412c7932
lemki <lwwwwwww0@gmail.com>
2017-11-02 07:50:57 +0200

    reformating homepage + styles

--- landing/views.py
+++ landing/views.py
@@ -8,5 +8,12 @@ class HomeView(TemplateView):
     template_name = "landing/home.html"
 
     def get(self, request, *args, **kwargs):
-        product_images = ProductImage.objects.filter(is_active=True, is_main=True)
-        return render(request, self.template_name, {'product_images': product_images})
+        product_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
+        product_images_phone = product_images.filter(product__category=1)
+        product_images_laptop = product_images.filter(product__category=2)
+        context = {
+            'product_images': product_images,
+            'product_images_phone': product_images_phone,
+            'product_images_laptop': product_images_laptop
+        }
+        return render(request, self.template_name, context=context)
